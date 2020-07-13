6044639a9eb845a3edf1d7a1efe42013980afaaa
dilanMD <deluxan.sgic@gmail.com>
2020-07-13 15:46:36 +0530

    Numberpad design

--- src/screens/Dialpad.js
+++ src/screens/Dialpad.js
@@ -5,9 +5,12 @@ import {
   Button,
   Alert,
   ActivityIndicator,
+  TouchableOpacity,
   StyleSheet,
 } from 'react-native';
 import {Input} from 'react-native-elements';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+
 import {useSelector, useDispatch} from 'react-redux';
 import axios from 'axios';
 
@@ -16,15 +19,17 @@ import {COLORS} from '../constants/color';
 import {BASE_URL, FIREBASE_SERVER_KEY} from '../constants/credentials';
 
 const Dialpad = (props) => {
-  const [opponent, setOpponent] = useState('0767795737');
+  const [opponent, setOpponent] = useState('');
   const {GET_FCM_REQUEST, GET_FCM_SUCCESS, GET_FCM_FAILURE} = USER;
-  const {primary} = COLORS;
+  const {primary, success, danger, white} = COLORS;
 
   const user = useSelector((state) => state.user);
   const dispatch = useDispatch();
   const {loading, userData, error} = user;
 
-  useEffect(() => console.log(userData), [userData]);
+  const handleNumber = (value) => {
+    setOpponent(opponent + value);
+  };
 
   const handleChange = (text) => {
     setOpponent(text);
@@ -80,16 +85,95 @@ const Dialpad = (props) => {
   };
 
   return (
+    // <View style={styles.container}>
+    //   <Input
+    //     placeholder="Mobile Number"
+    //     value={opponent}
+    //     onChangeText={(text) => handleChange(text)}
+    //     keyboardType="number-pad"
+    //   />
+    //   <Button title="Find FCM" onPress={findFcm} />
+    //   {/* <Text>{user !== null && user.token}</Text> */}
+    //   {loading && <ActivityIndicator size="large" color={primary} />}
+    // </View>
     <View style={styles.container}>
-      <Input
-        placeholder="Mobile Number"
-        value={opponent}
-        onChangeText={(text) => handleChange(text)}
-        keyboardType="number-pad"
-      />
-      <Button title="Find FCM" onPress={findFcm} />
-      {/* <Text>{user !== null && user.token}</Text> */}
-      {loading && <ActivityIndicator size="large" color={primary} />}
+      <View style={styles.displayContainer}>
+        <Text style={styles.display}>{opponent}</Text>
+      </View>
+      <View style={styles.buttons}>
+        <TouchableOpacity
+          style={styles.iconContainer}
+          onPress={() => handleNumber(1)}>
+          <Text style={styles.number}>1</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.iconContainer}
+          onPress={() => handleNumber(2)}>
+          <Text style={styles.number}>2</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.iconContainer}
+          onPress={() => handleNumber(3)}>
+          <Text style={styles.number}>3</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.buttons}>
+        <TouchableOpacity
+          style={styles.iconContainer}
+          onPress={() => handleNumber(4)}>
+          <Text style={styles.number}>4</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.iconContainer}
+          onPress={() => handleNumber(5)}>
+          <Text style={styles.number}>5</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.iconContainer}
+          onPress={() => handleNumber(6)}>
+          <Text style={styles.number}>6</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.buttons}>
+        <TouchableOpacity
+          style={styles.iconContainer}
+          onPress={() => handleNumber(7)}>
+          <Text style={styles.number}>7</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.iconContainer}
+          onPress={() => handleNumber(8)}>
+          <Text style={styles.number}>8</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.iconContainer}
+          onPress={() => handleNumber(9)}>
+          <Text style={styles.number}>9</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.buttons}>
+        <TouchableOpacity
+          style={styles.iconContainer}
+          onPress={() => handleNumber('+')}>
+          <Text style={styles.number}>+</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.iconContainer}
+          onPress={() => handleNumber(0)}>
+          <Text style={styles.number}>0</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.iconContainer}
+          onPress={() => handleNumber('#')}>
+          <Text style={styles.number}>#</Text>
+        </TouchableOpacity>
+      </View>
+      <TouchableOpacity
+        style={[styles.iconContainer, {backgroundColor: success}]}
+        onPress={findFcm}>
+        <MaterialIcons name="call" size={36} color={white} />
+      </TouchableOpacity>
+      {/* {loading && <ActivityIndicator size="large" color={primary} />} */}
     </View>
   );
 };
@@ -99,6 +183,33 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  display: {
+    fontSize: 30,
+    marginBottom: 30,
+  },
+  buttons: {
+    width: '100%',
+    // flex: 2,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  iconContainer: {
+    padding: 5,
+    width: 70,
+    height: 70,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 50,
+    marginHorizontal: 20,
+    marginVertical: 15,
+    backgroundColor: '#CCCCCC',
+  },
+  number: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#FFF',
   },
 });
 
